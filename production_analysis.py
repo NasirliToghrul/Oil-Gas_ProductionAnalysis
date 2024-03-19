82b7a0b744d626be745ca8eecf6a9f20c993471f
Toghrul Nasirli <toghrul.nasirli@im-par.de>
2024-03-19 13:12:54 +0300

    Code improvements

--- production_analysis.py
+++ production_analysis.py
@@ -1,79 +1,75 @@
 import math
 import matplotlib.pyplot as plt
-import pvtprops as pvt
+import pvtprops as pvt  # Importing custom module for PVT properties calculations
 
-#Input: Values are the examples values, should be changed according to the requirements
+# Input: Values are example values, should be changed according to the requirements
 
-a = 1
-b = 2
-c = 3
-d = 4
-e = 5
-f = 6
-g = 7
-
-tubing_ID = 1.995#inches
-wellhead_P = 100 + (g+1)*20 #psi
-liquid_rate = 1000 + (d+1)*200 #bbl/d
-GLR = 150 #scf/stb
-WC = 10 #%
-oil_gravity = 20 + (e+1)*2 #API
-water_gravity = 1.05
-gas_gravity = 0.68
-water_gravity = 0
-N2=0
-CO2=0
-H2S=0
-water_fvf = 1.02 #rb/stb
-tubing_depth = 4000 + (f+1)*500 #ft
-Twh = 80 #F
-T_gradient = 13/1000
+tubing_ID = 1.995  # inches
+wellhead_P = 260  # psi
+liquid_rate = 2000  # bbl/d
+GLR = 150  # scf/stb (Gas-Liquid Ratio)
+WC = 10  # % Water Cut
+oil_gravity = 32  # API (American Petroleum Institute)
+water_gravity = 1.05  # Water Specific Gravity
+gas_gravity = 0.68  # Gas Specific Gravity
+N2 = 0  # Nitrogen mole fraction (for PVT calculations)
+CO2 = 0  # Carbon Dioxide mole fraction (for PVT calculations)
+H2S = 0  # Hydrogen Sulfide mole fraction (for PVT calculations)
+water_fvf = 1.02  # rb/stb (Reservoir barrels per stock tank barrel)
+tubing_depth = 7500  # ft
+Twh = 80  # F (Wellhead temperature)
+T_gradient = 13 / 1000  # Temperature gradient per foot
 
 number_of_segments = 5000
-segment_length = tubing_depth/number_of_segments
-gas_production_rate = GLR*liquid_rate
-water_production_rate = (WC/100)*liquid_rate
-oil_production_rate = liquid_rate - water_production_rate
-producing_GOR =gas_production_rate/oil_production_rate
-sg_oil = pvt.calculate_sg_oil(oil_gravity)
-WOR = water_production_rate/oil_production_rate
-M = 350.17*(sg_oil+WOR*water_gravity) + producing_GOR*gas_gravity*0.0765
-numerator_reynolds = 1.4737*10**-5*M*oil_production_rate/(tubing_ID/12)
-friction_factor = 4*10**(1.444-2.5*math.log(numerator_reynolds))
-K = friction_factor*oil_production_rate**2*M**2/7.4137/10000000000/(tubing_ID/12)**5
-wellhead_GOR = pvt.calculate_GOR(producing_GOR,gas_gravity,wellhead_P,oil_gravity,Twh)
-wellhead_Bo = pvt.calculate_FVF(producing_GOR,gas_gravity,wellhead_P,oil_gravity, Twh)
-wellhead_Z = pvt.redlich_kwong_eos(wellhead_P, Twh, gas_gravity, CO2, H2S, N2)
-wellhead_Vm = 5.615*(wellhead_Bo+WOR*water_fvf) + (producing_GOR-wellhead_GOR)*(14.7/wellhead_P)*((Twh+460)/520)*wellhead_Z
-wellhead_density = M/wellhead_Vm
+segment_length = tubing_depth / number_of_segments  # ft
+gas_production_rate = GLR * liquid_rate  # scf/d (Standard cubic feet per day)
+water_production_rate = (WC / 100) * liquid_rate  # bbl/d
+oil_production_rate = liquid_rate - water_production_rate  # bbl/d
+producing_GOR = gas_production_rate / oil_production_rate  # Gas-Oil Ratio
+sg_oil = pvt.calculate_sg_oil(oil_gravity)  # Specific gravity of oil
+WOR = water_production_rate / oil_production_rate  # Water-Oil Ratio
+M = 350.17 * (sg_oil + WOR * water_gravity) + producing_GOR * gas_gravity * 0.0765  # Mixture density constant (lbm/ft^3)
+numerator_reynolds = 1.4737 * 10 ** -5 * M * oil_production_rate / (tubing_ID / 12)  # Reynolds number numerator
+friction_factor = 4 * 10 ** (1.444 - 2.5 * math.log(numerator_reynolds))  # Friction factor
+K = friction_factor * oil_production_rate ** 2 * M ** 2 / 7.4137 / 10000000000 / (tubing_ID / 12) ** 5  # K parameter
+wellhead_GOR = pvt.calculate_GOR(producing_GOR, gas_gravity, wellhead_P, oil_gravity, Twh)  # Wellhead Gas-Oil Ratio
+wellhead_Bo = pvt.calculate_FVF(producing_GOR, gas_gravity, wellhead_P, oil_gravity, Twh)  # Wellhead Formation Volume Factor
+wellhead_Z = pvt.redlich_kwong_eos(wellhead_P, Twh, gas_gravity, CO2, H2S, N2)  # Wellhead Z factor
+wellhead_Vm = 5.615 * (wellhead_Bo + WOR * water_fvf) + (producing_GOR - wellhead_GOR) * (14.7 / wellhead_P) * (
+            (Twh + 460) / 520) * wellhead_Z  # Wellhead mixture volume
+wellhead_density = M / wellhead_Vm  # Wellhead fluid density (lbm/ft^3)
 
-
-pressure_list = [wellhead_P]
-temperature_list = [Twh]
-GOR_list = [wellhead_GOR]
-density_list = [wellhead_density]
-depth_list = [0]
-average_density_list = []
-method = "CarrKobayashi"
+# Initializing lists to store pressure, temperature, GOR, density, and depth values
+pressure_list = [wellhead_P]  # psi
+temperature_list = [Twh]  # F
+GOR_list = [wellhead_GOR]  # Gas-Oil Ratio
+density_list = [wellhead_density]  # lbm/ft^3
+depth_list = [0]  # ft
+average_density_list = []  # lbm/ft^3
+method = "CarrKobayashi"  # Calculation method (unused in this script)
 
 depth = 0
 previous_density = wellhead_density
 P_prime = wellhead_P
-T_prime = Twh + segment_length*T_gradient
+T_prime = Twh + segment_length * T_gradient
 depth += segment_length
 
-while depth<=tubing_depth:
+# Iterating through depth segments to calculate pressure profiles
+while depth <= tubing_depth:
     while True:
-        GOR_prime = pvt.calculate_GOR(producing_GOR,gas_gravity,P_prime,oil_gravity,T_prime)
+        # Calculating parameters at current depth
+        GOR_prime = pvt.calculate_GOR(producing_GOR, gas_gravity, P_prime, oil_gravity, T_prime)
         compressibility = pvt.redlich_kwong_eos(P_prime, T_prime, gas_gravity, CO2, H2S, N2)
         FVF_oil = pvt.calculate_FVF(producing_GOR, gas_gravity, P_prime, oil_gravity, T_prime)
-        V = 5.615*(FVF_oil + WOR*water_fvf) + (producing_GOR - GOR_prime)*(14.7/P_prime)*((T_prime+460)/520)*compressibility
-        density = M/V
-        density_average = (density+previous_density)/2
-        deltaP = (density_average + (K/density_average))*(segment_length/144)
-        P_estimated = P_prime + deltaP
-        error = abs((P_estimated-P_prime)/P_estimated)
-        if error <=0.001:
+        V = 5.615 * (FVF_oil + WOR * water_fvf) + (producing_GOR - GOR_prime) * (14.7 / P_prime) * (
+            (T_prime + 460) / 520) * compressibility  # Mixture volume
+        density = M / V  # Fluid density
+        density_average = (density + previous_density) / 2  # Average density
+        deltaP = (density_average + (K / density_average)) * (segment_length / 144)  # Pressure change
+        P_estimated = P_prime + deltaP  # Estimated pressure
+        error = abs((P_estimated - P_prime) / P_estimated)  # Error calculation
+        if error <= 0.001:
+            # Storing calculated values
             pressure_list.append(P_estimated)
             P_prime = P_estimated
             depth_list.append(depth)
@@ -81,34 +77,33 @@ while depth<=tubing_depth:
             density_list.append(density)
             previous_density = density
             temperature_list.append(T_prime)
-            T_prime += segment_length*T_gradient
+            T_prime += segment_length * T_gradient
             GOR_list.append(GOR_prime)
             average_density_list.append(density_average)
             break
         else:
             P_prime = P_estimated
 
-
-            
+# Plotting pressure profile
 plt.plot(pressure_list, depth_list)
 plt.title("Pressure Profile")
-plt.xlabel("Pressure, psia")
-plt.ylabel("Depth, feets")
+plt.xlabel("Pressure (psia)")
+plt.ylabel("Depth (feet)")
 plt.gca().invert_yaxis()
 plt.show()
 
-
+# Plotting average density distribution
 plt.plot(average_density_list, depth_list[1:])
 plt.title("Average Density Distribution")
-plt.xlabel("Average Density")
-plt.ylabel("Depth")
+plt.xlabel("Average Density (lbm/ft^3)")
+plt.ylabel("Depth (feet)")
 plt.gca().invert_yaxis()
 plt.show()
 
+# Plotting Gas In Solution profile
 plt.plot(GOR_list, depth_list)
 plt.title("Gas In Solution Profile")
-plt.xlabel("Gas In Solution")
-plt.ylabel("Depth")
+plt.xlabel("Gas In Solution (scf/stb)")
+plt.ylabel("Depth (feet)")
 plt.gca().invert_yaxis()
 plt.show()
-
--- pvtprops.py
+++ pvtprops.py
@@ -1,60 +1,170 @@
 import numpy as np
 
-def critical_presssure_Sutton(sg_gas):
-    Ppc = 756.8 - 131*sg_gas - 3.6*(sg_gas**2)
+def critical_pressure_Sutton(sg_gas):
+    """Calculate the critical pressure of a gas using Sutton's correlation.
+
+    Args:
+        sg_gas (float): Specific gravity of the gas.
+
+    Returns:
+        float: Critical pressure of the gas.
+    """
+    Ppc = 756.8 - 131 * sg_gas - 3.6 * (sg_gas ** 2)
     return Ppc
 
 def critical_temperature_Sutton(sg_gas):
-    Tpc = 169.2 + 349*sg_gas - 74*(sg_gas**2)
+    """Calculate the critical temperature of a gas using Sutton's correlation.
+
+    Args:
+        sg_gas (float): Specific gravity of the gas.
+
+    Returns:
+        float: Critical temperature of the gas.
+    """
+    Tpc = 169.2 + 349 * sg_gas - 74 * (sg_gas ** 2)
     return Tpc
 
 def carr_kobayashi_temperature(sg_gas, CO2, H2S, N2):
-    temperature_corrected = critical_temperature_Sutton(sg_gas) - (80*CO2) + (130*H2S) - (250*N2)
+    """Calculate the corrected temperature using Carr-Kobayashi method.
+
+    Args:
+        sg_gas (float): Specific gravity of the gas.
+        CO2 (float): Carbon Dioxide mole fraction.
+        H2S (float): Hydrogen Sulfide mole fraction.
+        N2 (float): Nitrogen mole fraction.
+
+    Returns:
+        float: Corrected temperature.
+    """
+    temperature_corrected = critical_temperature_Sutton(sg_gas) - (80 * CO2) + (130 * H2S) - (250 * N2)
     return temperature_corrected
 
 def carr_kobayashi_pressure(sg_gas, CO2, H2S, N2):
-    pressure_corrected = critical_presssure_Sutton(sg_gas) + (440*CO2) + (600*H2S) - (170*N2)
+    """Calculate the corrected pressure using Carr-Kobayashi method.
+
+    Args:
+        sg_gas (float): Specific gravity of the gas.
+        CO2 (float): Carbon Dioxide mole fraction.
+        H2S (float): Hydrogen Sulfide mole fraction.
+        N2 (float): Nitrogen mole fraction.
+
+    Returns:
+        float: Corrected pressure.
+    """
+    pressure_corrected = critical_pressure_Sutton(sg_gas) + (440 * CO2) + (600 * H2S) - (170 * N2)
     return pressure_corrected
 
+def redlich_kwong_eos(P, T, sg_gas, CO2, H2S, N2):
+    """Calculate Z factor using Redlich-Kwong equation of state.
 
-def redlich_kwong_eos(P, T, sg_gas, CO2, H2S, N2):#INSERT T AS FAHRENHEIT
+    Args:
+        P (float): Pressure (psia).
+        T (float): Temperature (Fahrenheit).
+        sg_gas (float): Specific gravity of the gas.
+        CO2 (float): Carbon Dioxide mole fraction.
+        H2S (float): Hydrogen Sulfide mole fraction.
+        N2 (float): Nitrogen mole fraction.
+
+    Returns:
+        float: Z factor.
+    """
     R = 10.73
-    a = (0.42727*(R**2)*(carr_kobayashi_temperature(sg_gas, CO2, H2S, N2)**2.5))/(carr_kobayashi_pressure(sg_gas, CO2, H2S, N2))
-    b = 0.08664*(R*carr_kobayashi_temperature(sg_gas, CO2, H2S, N2))/(carr_kobayashi_pressure(sg_gas, CO2, H2S, N2))
-    A = (a*P)/((R**2)*((T+460)**2.5))
-    B = (b*P)/(R*(T+460))
-    eqn = np.poly1d([1,-1,(A-B-(B**2)), -A*B])
+    a = (0.42727 * (R ** 2) * (carr_kobayashi_temperature(sg_gas, CO2, H2S, N2) ** 2.5)) / (
+                carr_kobayashi_pressure(sg_gas, CO2, H2S, N2))
+    b = 0.08664 * (R * carr_kobayashi_temperature(sg_gas, CO2, H2S, N2)) / (
+                carr_kobayashi_pressure(sg_gas, CO2, H2S, N2))
+    A = (a * P) / ((R ** 2) * ((T + 460) ** 2.5))
+    B = (b * P) / (R * (T + 460))
+    eqn = np.poly1d([1, -1, (A - B - (B ** 2)), -A * B])
     for i in eqn.roots:
         if np.imag(i) == 0 and i > 0:
             solution = i
     return solution.real
 
-def Lee_Gonzales_Eakin(P,T,sg_gas,CO2,H2S,molecular_weight,method, N2):#INSERT T AS FAHRENHEIT
-    density = molecular_weight*P/(redlich_kwong_eos(P,T,sg_gas,CO2,H2S,method,N2)*10.73*(T+460))
-    X = 3.5 + (986/(T+460)) + 0.01*molecular_weight
-    Y = 2.4 - 0.2*X
-    K = (9.4+0.02*molecular_weight)*((T+460)**1.5)*(10**-4)/(209 + 19*molecular_weight + (T+460))
-    viscosity = K*np.exp(X*((density/62.4)**Y))
+def Lee_Gonzales_Eakin(P, T, sg_gas, CO2, H2S, molecular_weight, method, N2):
+    """Calculate viscosity using Lee-Gonzales-Eakin correlation.
+
+    Args:
+        P (float): Pressure (psia).
+        T (float): Temperature (Fahrenheit).
+        sg_gas (float): Specific gravity of the gas.
+        CO2 (float): Carbon Dioxide mole fraction.
+        H2S (float): Hydrogen Sulfide mole fraction.
+        molecular_weight (float): Molecular weight of the gas mixture.
+        method (str): Unused parameter.
+        N2 (float): Nitrogen mole fraction.
+
+    Returns:
+        float: Viscosity (cp).
+    """
+    density = molecular_weight * P / (
+                redlich_kwong_eos(P, T, sg_gas, CO2, H2S, N2) * 10.73 * (T + 460))
+    X = 3.5 + (986 / (T + 460)) + 0.01 * molecular_weight
+    Y = 2.4 - 0.2 * X
+    K = (9.4 + 0.02 * molecular_weight) * ((T + 460) ** 1.5) * (10 ** -4) / (
+                209 + 19 * molecular_weight + (T + 460))
+    viscosity = K * np.exp(X * ((density / 62.4) ** Y))
     return viscosity
 
+def calculate_Pb(Rsb, sg_gas, T, API):
+    """Calculate bubble point pressure using Standing correlation.
 
-def calculate_Pb(Rsb,sg_gas,T,API):#StandingCorrelation
-    Pb = 18.2*(((Rsb/sg_gas)**0.83)*10**(0.00091*T-0.0125*API)-1.4)
+    Args:
+        Rsb (float): Solution Gas-Oil Ratio (scf/stb).
+        sg_gas (float): Specific gravity of the gas.
+        T (float): Temperature (Fahrenheit).
+        API (float): API gravity of the oil.
+
+    Returns:
+        float: Bubble point pressure (psia).
+    """
+    Pb = 18.2 * (((Rsb / sg_gas) ** 0.83) * 10 ** (0.00091 * T - 0.0125 * API) - 1.4)
     return Pb
 
-def calculate_GOR(Rsb,sg_gas,P,API,T):#Standing Correlation
-    if P < calculate_Pb(Rsb,sg_gas,T,API):
-        GOR = sg_gas*((((P/18.2)+1.4)*10**(0.0125*API-0.00091*T))**1.2048)
+def calculate_GOR(Rsb, sg_gas, P, API, T):
+    """Calculate Gas-Oil Ratio using Standing correlation.
+
+    Args:
+        Rsb (float): Solution Gas-Oil Ratio (scf/stb).
+        sg_gas (float): Specific gravity of the gas.
+        P (float): Pressure (psia).
+        API (float): API gravity of the oil.
+        T (float): Temperature (Fahrenheit).
+
+    Returns:
+        float: Gas-Oil Ratio (scf/stb).
+    """
+    if P < calculate_Pb(Rsb, sg_gas, T, API):
+        GOR = sg_gas * ((((P / 18.2) + 1.4) * 10 ** (0.0125 * API - 0.00091 * T)) ** 1.2048)
     else:
         GOR = Rsb
     return GOR
 
-def calculate_FVF(Rsb,sg_gas,P,API,T):#Standing Correlation
-    GOR = calculate_GOR(Rsb,sg_gas,P,API,T)
-    FVF = 0.972+0.000147*((GOR*((sg_gas/calculate_sg_oil(API))**0.5)+1.25*T)**1.175)
+def calculate_FVF(Rsb, sg_gas, P, API, T):
+    """Calculate Formation Volume Factor using Standing correlation.
+
+    Args:
+        Rsb (float): Solution Gas-Oil Ratio (scf/stb).
+        sg_gas (float): Specific gravity of the gas.
+        P (float): Pressure (psia).
+        API (float): API gravity of the oil.
+        T (float): Temperature (Fahrenheit).
+
+    Returns:
+        float: Formation Volume Factor (rb/stb).
+    """
+    GOR = calculate_GOR(Rsb, sg_gas, P, API, T)
+    FVF = 0.972 + 0.000147 * ((GOR * ((sg_gas / calculate_sg_oil(API)) ** 0.5) + 1.25 * T) ** 1.175)
     return FVF
 
 def calculate_sg_oil(API):
-    sg_oil = 141.5/(API+131.5)
-    return sg_oil
+    """Calculate Specific Gravity of Oil using API gravity.
 
+    Args:
+        API (float): API gravity of the oil.
+
+    Returns:
+        float: Specific Gravity of Oil.
+    """
+    sg_oil = 141.5 / (API + 131.5)
+    return sg_oil
